80143e90f64170b64444bc36ca563c33385c82fe
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-04-03 13:37:11 +0300

    add TestController

---
+++ src/main/java/com/example/springboot/TestController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TestController {
+    @GetMapping(value = "/test")
+    public ModelAndView getText() {
+        ModelAndView result = new ModelAndView("test");
+        return result;
+    }
+}
